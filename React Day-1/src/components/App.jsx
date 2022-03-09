7a683c54d77b3e4be6a855f1e0bec40ab30e5d3a
ishan17vk <67632832+ishan17vk@users.noreply.github.com>
2022-03-09 18:38:51 +0530

    Add files via upload
    Basic React using Components.

---
+++ React Day-1/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <ul className="list">
+        <li>Cricket</li>
+        <li>Tennis</li>
+        <li>Formula 1</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
