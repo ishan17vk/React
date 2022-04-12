492e4852945448a9e5cf29c06f54e8fe2c2bc29f
ishan17vk <67632832+ishan17vk@users.noreply.github.com>
2022-04-12 17:04:39 +0530

    Add files via upload

---
+++ components/Validation/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, Handle } from 'react-flow-renderer';
+
+import './index.css';
+
+const initialNodes = [
+  { id: '0', type: 'custominput', position: { x: 0, y: 150 } },
+  { id: 'A', type: 'customnode', position: { x: 250, y: 0 } },
+  { id: 'B', type: 'customnode', position: { x: 250, y: 150 } },
+  { id: 'C', type: 'customnode', position: { x: 250, y: 300 } },
+];
+
+const isValidConnection = (connection) => connection.target === 'B';
+const onConnectStart = (event, { nodeId, handleType }) => console.log('on connect start', { nodeId, handleType });
+const onConnectStop = (event) => console.log('on connect stop', event);
+const onConnectEnd = (event) => console.log('on connect end', event);
+
+const CustomInput = () => (
+  <>
+    <div>Only connectable with B</div>
+    <Handle type="source" position="right" isValidConnection={isValidConnection} />
+  </>
+);
+
+const CustomNode = ({ id }) => (
+  <>
+    <Handle type="target" position="left" isValidConnection={isValidConnection} />
+    <div>{id}</div>
+    <Handle type="source" position="right" isValidConnection={isValidConnection} />
+  </>
+);
+
+const nodeTypes = {
+  custominput: CustomInput,
+  customnode: CustomNode,
+};
+
+const HorizontalFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const onConnect = (params) => {
+    console.log('on connect', params);
+    setEdges((eds) => addEdge(params, eds));
+  };
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      selectNodesOnDrag={false}
+      className="validationflow"
+      nodeTypes={nodeTypes}
+      onConnectStart={onConnectStart}
+      onConnectStop={onConnectStop}
+      onConnectEnd={onConnectEnd}
+      fitView
+      attributionPosition="bottom-left"
+    />
+  );
+};
+
+export default HorizontalFlow;
